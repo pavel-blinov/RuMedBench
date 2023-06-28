2567d7169b4ea97ecbd8520b237a45f67a2c11de
univanxx <wchhiaarid@yandex.ru>
2023-06-28 18:30:02 +0300

    fixes added

--- lb_submissions/SAI/ECGBinary/training.py
+++ lb_submissions/SAI/ECGBinary/training.py
@@ -63,7 +63,7 @@ def main(args):
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Baselines training script (1d-CNN)')
-    parser.add_argument('data_path', help='dataset path', type=str)
+    parser.add_argument('data_path', help='dataset path (path to the folder containing test and train subfolders)', type=str)
     parser.add_argument('model_path', help='path to save the model and logs', type=str)
     parser.add_argument('--cuda_id', help='CUDA device number on a single GPU; use -1 if yu want to work on CPU', type=int, default=1)
     parser.add_argument('--k', help='number of positive examples for class', type=int, default=11)
